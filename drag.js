693e4d3054afbd5fae0a119561508a704e469754
Nicholas Napoli <nicknapoli82@gmail.com>
2019-05-08 21:51:39 -0700

    Not much- Just rollin - Added some css junk

--- drag.js
+++ drag.js
@@ -1,6 +1,6 @@
 let dropArea = document.getElementById('drop-area');
 
-//['dragenter', 'dragover', 'dragleave',
+//['dragenter', 'dragleave'] If you wanna go banannas
 ['dragover', 'drop'].forEach(eventName => {
      dropArea.addEventListener(eventName, preventDefaults, false);
 });
@@ -36,9 +36,6 @@ function handleDrop(e) {
 }
 
 function handleFiles(files) {
-    // files is the array of files obtained
-    // only one file should be loaded at a time
-    // [TODO]Nick: If more than one file is loaded then let user pick one
     files = [...files];
     files.forEach(previewFile);
 }
@@ -47,34 +44,8 @@ function previewFile(file) {
     let reader = new FileReader();
     reader.readAsText(file);
     reader.onloadend = function() {
-	//	let img = document.createElement('img');
-	//	img.src = reader.result;
 	let text = document.createElement('p');
 	text.innerText = reader.result;
-	document.getElementById('gallery').appendChild(text);
+	document.getElementById('doc').appendChild(text);
     };
 }
-
-// function dropHandler(ev) {
-//     console.log('File(s) dropped');
-
-//     // Prevent default behavior (Prevent file from being opened)
-//     ev.stopPropagation();
-//     ev.preventDefault();
-
-//     if (ev.dataTransfer.items) {
-// 	// Use DataTransferItemList interface to access the file(s)
-// 	for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-// 	    // If dropped items aren't files, reject them
-// 	    if (ev.dataTransfer.items[i].kind === 'file') {
-// 		let file = ev.dataTransfer.items[i].getAsFile();
-// 		console.log('... file[' + i + '].name = ' + file.name);
-// 	    }
-// 	}
-//     } else {
-// 	// Use DataTransfer interface to access the file(s)
-// 	for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-// 	    console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-// 	}
-//     }
-// }
