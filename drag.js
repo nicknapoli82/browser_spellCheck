4f76b382188c04940ae9c54283d10696a8a6bafd
Nicholas Napoli <nicknapoli82@gmail.com>
2019-05-09 17:29:17 -0700

    Added code to process spell checking

--- drag.js
+++ drag.js
@@ -1,3 +1,49 @@
+// This section handles the actual spell check of file
+
+// Lets go ahead an load a dictionary into the gloabl objects
+// I can't think of a reason not to do this yet, but I may narrow
+// scope later 
+const VALID_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\''];
+
+function loadDict(name) {
+    let temp = fs.readFileSync(name).toString().split('\n');
+    let dict = {};
+
+    while(temp.length) {
+	let t = temp.pop();
+	dict[t] = null;
+    }
+    
+    return function(word) {
+	if (word in dict) {
+	    return true;
+	}
+	return false;
+    };
+}
+
+function read_word_buf(buf) {
+    let word_found = '';
+    while(!VALID_LETTERS.includes(buf.buffer[buf.cursor]) && buf.cursor < buf.buffer.length) {
+    	buf.cursor++;
+    }
+    while(VALID_LETTERS.includes(buf.buffer[buf.cursor])) {
+	word_found += buf.buffer[buf.cursor];
+	buf.cursor++;
+    }
+    if (word_found === '') return false;
+    return word_found;
+}
+
+function read_file_toBuffer(path) {
+    // Let try to get a word?
+    let test = fs.readFileSync(path);
+    return { buffer: test.toString(), cursor: 0 };
+}
+
+
+// This section handles load of text file to be checked
 let dropArea = document.getElementById('drop-area');
 
 //['dragenter', 'dragleave'] If you wanna go banannas
@@ -45,7 +91,7 @@ function previewFile(file) {
     reader.readAsText(file);
     reader.onloadend = function() {
 	let text = document.createElement('p');
-	text.innerText = reader.result;
+	text.innerHTML = reader.result;
 	document.getElementById('doc').appendChild(text);
     };
 }
