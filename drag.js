2b13200f271bb2eabb5256c8330ff0b9bd6679d4
Nicholas Napoli <nicknapoli82@gmail.com>
2019-05-12 22:23:55 -0700

    Browser now displays misspelled words like Node version

--- drag.js
+++ drag.js
@@ -6,7 +6,29 @@
 const VALID_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 		       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\''];
 
-let sticky_dict;
+class MisspelledData {
+    constructor () {
+//	this.SPANInsert = `<span id="${currentID}">${word}</span>`;
+    }
+    
+    addWord(word) {
+        if (word in Object.keys(this)) {
+	    this.word.push( {} ); // What is the data that I need here?
+        }
+	else { this.word = [ new Word() ]; } // What is the data that I need here?
+    }
+}
+
+let stickyDict;
+let wordsMisspelled = new MisspelledData();
+let totalMisspelled = 0;
+
+function Word() {
+    this.rawLocation;
+    this.HTMLLocation;
+    this.ID;
+    return this;
+}
 
 function loadDict(file) {
     //    let temp = fs.readFileSync(name).toString().split('\n');
@@ -23,17 +45,17 @@ function loadDict(file) {
 	console.log(dict);
     };
 
-    sticky_dict =  function(word) {
+    document.getElementById('load_dict').style.display = 'none';
+
+    stickyDict = function(word) {
     	if (word in dict) {
     	    return true;
     	}
     	return false;
     };
-
-    document.getElementById('load_dict').style.display = 'none';
 }
 
-function read_word_buf(buf) {
+function readWordBuf(buf) {
     let word_found = '';
     while(!VALID_LETTERS.includes(buf.buffer[buf.cursor]) && buf.cursor < buf.buffer.length) {
     	buf.cursor++;
@@ -46,14 +68,34 @@ function read_word_buf(buf) {
     return word_found;
 }
 
-function read_file_toBuffer(path) {
-    // Let try to get a word?
-    let test = fs.readFileSync(path);
-    return { buffer: test.toString(), cursor: 0 };
+// This is the thing that actually determines what a word is, and passes
+// to read_word_buf to be checked. Consider this the main spell check loop
+// This function will probably end up being called as an event to allow
+// the user to start working on document sooner. Some documents can take
+// up to 5 seconds to load. Thats a long time.
+function readFile(buf) {
+
+    while(buf.cursor < buf.buffer.length) {
+	let word = readWordBuf(buf);
+	if (word) {
+	    if (!stickyDict(word.toLowerCase())) {
+		console.log(word);
+		totalMisspelled++;
+	    }
+	}
+    }
+
+    // This will be used lated, just a reminder to myself
+    
+    // let text = document.createElement('p');
+    // text.innerText = reader.result;
+    // document.getElementById('doc').appendChild(text);
 }
 
 
-// This section handles load of text file to be checked
+//////////////////////////////////////////////////////////
+// This section handles load of text file to be checked //
+//////////////////////////////////////////////////////////
 let dropArea = document.getElementById('drop-area');
 
 //['dragenter', 'dragleave'] If you wanna go banannas
@@ -100,8 +142,6 @@ function previewFile(file) {
     let reader = new FileReader();
     reader.readAsText(file);
     reader.onloadend = function() {
-	let text = document.createElement('p');
-	text.innerText = reader.result;
-	document.getElementById('doc').appendChild(text);
+	readFile( { buffer: reader.result, cursor: 0 } );
     };
 }
